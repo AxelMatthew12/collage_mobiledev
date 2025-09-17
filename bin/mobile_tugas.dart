2a7a33bdcbc5ab44c81e67aed3990e4a94e94e32
Axel <axelomatthew.terang@gmail.com>
2025-09-17 20:55:21 +0700

    first commit

---
+++ bin/mobile_tugas.dart
@@ -0,0 +1,6 @@
+import 'package:mobile_tugas/mobile_tugas.dart' as mobile_tugas;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_tugas.calculate()}!');
+}
+
---
+++ test/mobile_tugas_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_tugas/mobile_tugas.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
